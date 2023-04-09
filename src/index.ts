0103d0a5ef13933dddbe0d97e93fd87855f1bac9
Kyan <weiliang.chia@gmail.com>
2023-04-09 16:21:13 +0800

    Initial set up

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import cors from 'cors';
+import express from 'express';
+import * as dotenv from "dotenv";
+import helmet from "helmet";
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  console.log(`Error to get ports`);
+    process.exit(1);
+ }
+
+ const PORT: number = parseInt(process.env.PORT as string, 10);
+ 
+ const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+const server = app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+// Send message for default URL
+app.get('/', (req, res) => res.send('Welcome to NodeJs App using TypeScript'));
